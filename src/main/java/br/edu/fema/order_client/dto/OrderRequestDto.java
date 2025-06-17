b00eb4bd691257f2c8c2d6f797b05f67e691d62c
João Lucas Barboza <joaolucasbarbozaa@gmail.com>
2025-06-17 09:19:09 -0300

    feat: add new attributes

--- src/main/java/br/edu/fema/order_client/dto/OrderRequestDto.java
+++ src/main/java/br/edu/fema/order_client/dto/OrderRequestDto.java
@@ -1,6 +1,11 @@
 package br.edu.fema.order_client.dto;
 
+import java.time.LocalDateTime;
+
 public record OrderRequestDto(
-        String product,
-        int quantity
+        String product_name,
+        int quantity,
+        float unit_price,
+        LocalDateTime order_date,
+        PaymentDto payment
 ) {}
--- src/main/java/br/edu/fema/order_client/service/OrderServiceImpl.java
+++ src/main/java/br/edu/fema/order_client/service/OrderServiceImpl.java
@@ -1,8 +1,6 @@
 package br.edu.fema.order_client.service;
 
-import br.edu.fema.grpc.OrderRequest;
-import br.edu.fema.grpc.OrderResponse;
-import br.edu.fema.grpc.OrderServiceGrpc;
+import br.edu.fema.grpc.*;
 import br.edu.fema.order_client.dto.OrderRequestDto;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
@@ -14,10 +12,23 @@ public class OrderServiceImpl  {
     private OrderServiceGrpc.OrderServiceBlockingStub serviceBlockingStub;
 
     public OrderResponse createOrder(OrderRequestDto request) {
+
+        CreditCard creditCard = CreditCard.newBuilder()
+                .setInstalments(request.payment().credit_card().instalments())
+                .setCardBrand(request.payment().credit_card().card_brand())
+                .build();
+
+        Payment payment = Payment.newBuilder()
+                .setMethod(request.payment().method())
+                .setCreditCard(creditCard)
+                .build();
+
         OrderRequest order = OrderRequest
                 .newBuilder()
-                .setProduct(request.product())
+                .setProductName(request.product_name())
                 .setQuantity(request.quantity())
+                .setUnitPrice(request.unit_price())
+                .setPayment(payment)
                 .build();
 
         return serviceBlockingStub.createOrder(order);
